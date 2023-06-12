f794fcc86520efdca8708273834b528e205202d2
Michal Stachnik <michal@prl.one>
2023-06-12 12:09:27 -0400

    added react-ga4

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import ReactGA from 'react-ga4';
+import './index.css';
+
+ReactGA.initialize('G-6FEC3JDXV6');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
