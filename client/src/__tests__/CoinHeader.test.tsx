5b5f82c560fb35f86e9f370f056fa94897339d93
Michal Stachnik <michal@prl.one>
2023-06-13 14:25:39 -0400

    added cleanup and mock clicking test for CoinHeader

--- client/src/__tests__/CoinHeader.test.tsx
+++ client/src/__tests__/CoinHeader.test.tsx
@@ -1,14 +1,11 @@
-import { it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { it, expect, vi, afterEach } from 'vitest';
+import { render, screen, cleanup, fireEvent } from '@testing-library/react';
 import CoinHeader from '../components/CoinHeader';
 
 /**
  * @vitest-environment happy-dom
  */
-
-it('should work', () => {
-  expect(1).toBe(1);
-});
+afterEach(() => cleanup());
 
 it('should render', () => {
   render(
@@ -23,4 +20,20 @@ it('should render', () => {
 
   const element = screen.getByTestId('coin-header');
   expect(element).toBeTruthy();
+});
+
+it('can fire its callback', () => {
+  const mockCb = vi.fn();
+  render(
+    <CoinHeader
+      selectedCoin={null}
+      liveCoinWatchData={null}
+      isLoading={false}
+      timeInterval="24hr"
+      onIntervalClick={mockCb}
+    />
+  );
+
+  const toggleButtonElement = screen.getByText('24hr');
+  fireEvent.click(toggleButtonElement);
 });
