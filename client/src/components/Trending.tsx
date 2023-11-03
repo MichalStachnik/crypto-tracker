7f32f0d2a97d577f38acbb35591515fece33ae1e
Michal Stachnik <michal@prl.one>
2023-11-03 18:51:05 -0400

    checking for array length

--- client/src/components/Trending.tsx
+++ client/src/components/Trending.tsx
@@ -7,7 +7,7 @@ const Trending = () => {
   useEffect(() => {
     fetch('/api/trending')
       .then((res) => res.json())
-      .then((res) => console.log('res', setTrendingCoins(res.coins)))
+      .then((res) => setTrendingCoins(res.coins))
       .catch((err) => console.error('Error', err));
   }, []);
 
@@ -15,23 +15,24 @@ const Trending = () => {
     <Box component="div">
       <Typography>Top 7 Trending Coins</Typography>
       <Box display="flex" flexDirection="column" component="div">
-        {trendingCoins.map((coin: any, index) => {
-          return (
-            <Box
-              key={coin.item.id}
-              display="flex"
-              justifyContent="space-around"
-              component="div"
-            >
-              <Typography>{index + 1}</Typography>
-              <Avatar src={coin.item.thumb} sx={{ width: 20, height: 20 }} />
-              <Typography>{coin.item.name}</Typography>
-              <Typography bgcolor="secondary">
-                #{coin.item.market_cap_rank}
-              </Typography>
-            </Box>
-          );
-        })}
+        {trendingCoins.length &&
+          trendingCoins.map((coin: any, index) => {
+            return (
+              <Box
+                key={coin.item.id}
+                display="flex"
+                justifyContent="space-around"
+                component="div"
+              >
+                <Typography>{index + 1}</Typography>
+                <Avatar src={coin.item.thumb} sx={{ width: 20, height: 20 }} />
+                <Typography>{coin.item.name}</Typography>
+                <Typography bgcolor="secondary">
+                  #{coin.item.market_cap_rank}
+                </Typography>
+              </Box>
+            );
+          })}
       </Box>
     </Box>
   );
