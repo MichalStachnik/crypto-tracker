e856cdf412550ea979f4c7a0b473f4d6cb5cbd64
Michal Stachnik <michal@prl.one>
2023-11-03 19:05:17 -0400

    added ternary check

--- client/src/components/Trending.tsx
+++ client/src/components/Trending.tsx
@@ -15,24 +15,28 @@ const Trending = () => {
     <Box component="div">
       <Typography>Top 7 Trending Coins</Typography>
       <Box display="flex" flexDirection="column" component="div">
-        {trendingCoins.length &&
-          trendingCoins.map((coin: any, index) => {
-            return (
-              <Box
-                key={coin.item.id}
-                display="flex"
-                justifyContent="space-around"
-                component="div"
-              >
-                <Typography>{index + 1}</Typography>
-                <Avatar src={coin.item.thumb} sx={{ width: 20, height: 20 }} />
-                <Typography>{coin.item.name}</Typography>
-                <Typography bgcolor="secondary">
-                  #{coin.item.market_cap_rank}
-                </Typography>
-              </Box>
-            );
-          })}
+        {trendingCoins.length
+          ? trendingCoins.map((coin: any, index) => {
+              return (
+                <Box
+                  key={coin.item.id}
+                  display="flex"
+                  justifyContent="space-around"
+                  component="div"
+                >
+                  <Typography>{index + 1}</Typography>
+                  <Avatar
+                    src={coin.item.thumb}
+                    sx={{ width: 20, height: 20 }}
+                  />
+                  <Typography>{coin.item.name}</Typography>
+                  <Typography bgcolor="secondary">
+                    #{coin.item.market_cap_rank}
+                  </Typography>
+                </Box>
+              );
+            })
+          : null}
       </Box>
     </Box>
   );
